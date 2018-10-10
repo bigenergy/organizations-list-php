461b718b32b4ba8bd7cc9047a7cfdf10740ebf7d
Big Energy <andre90094@gmail.com>
2018-10-10 23:33:45 +0300

    Добавил функцию обработки редактирования организации

--- engine/classes/API.class.php
+++ engine/classes/API.class.php
@@ -38,9 +38,10 @@ class API
             echo $e->getMessage();
         }
 
-        echo json_encode($result_json, JSON_UNESCAPED_UNICODE);
+        return json_encode($result_json, JSON_UNESCAPED_UNICODE);
     }
     // функция проверки на сущестование такого КПП или ИНН в базе
+
     public function check_org($orgtype, $INN, $KPP)
     {
         if ($orgtype = 'IP') {
@@ -136,6 +137,78 @@ class API
             if (!$check_already) {
                 $stmt->execute();
                 $result_json = array('success' => 'Организация с названием ' . $orgname . ' успешно добавлена!');
+                return json_encode($result_json, JSON_UNESCAPED_UNICODE);
+            } else {
+                $result_json = array('already' => 'Организация с таким ИНН или КПП уже зарегистрирована в базе');
+                return json_encode($result_json, JSON_UNESCAPED_UNICODE);
+            }
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+
+    }
+
+    // функция редактирования организации
+
+    public function edit_org($orgID, $orgname, $orgtype, $INN, $KPP, $phone, $email)
+    {
+        if ($orgtype == 'IP') {
+            if (empty($orgID) || empty($orgname) || empty($orgtype) || empty($INN) || empty($phone)) return false;
+        } elseif ($orgtype == 'UL') {
+            if (empty($orgID) || empty($orgname) || empty($orgtype) || empty($INN) || empty($KPP) || empty($phone)) return false;
+        }
+
+        $ru = preg_match('~[а-яё]+~iu',$orgname);
+        $en = preg_match('~[a-z]+~i', $orgname);
+
+        if (! ($ru ^ $en) ) {
+            $result_json = array('errorlang' => 'Наименование организации должно использовать или кириллицу, или латиницу!');
+            echo json_encode($result_json, JSON_UNESCAPED_UNICODE);
+            return false;
+        }
+
+        if (strlen($orgname) > 255) {
+            $result_json = array('error' => 'Наименование организации превышает установленный лимит в 255 символов!');
+            echo json_encode($result_json, JSON_UNESCAPED_UNICODE);
+            return false;
+        }
+        if ($email != NULL) {
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                // invalid emailaddress
+                $result_json = array('error' => 'Неверный E-Mail адрес!');
+                echo json_encode($result_json, JSON_UNESCAPED_UNICODE);
+                return false;
+            }
+        }
+        if($orgtype == 'IP') {
+            if(strlen($INN) !== 12) {
+                $result_json = array('error' => 'ИНН должен составлять 12 символов!');
+                echo json_encode($result_json, JSON_UNESCAPED_UNICODE);
+                return false;
+            }
+        } elseif($orgtype == 'UL') {
+            if(strlen($INN) !== 10 && strlen($KPP) !== 9) {
+                $result_json = array('error' => 'ИНН должен составлять 10 символов, а КПП должен составлять 9!');
+                echo json_encode($result_json, JSON_UNESCAPED_UNICODE);
+                return false;
+            }
+        }
+
+        try {
+            $sql = "UPDATE `organizations` SET `name`=:orgname,`type`=:orgtype,`inn`=:INN,`kpp`=:KPP,`phone`=:phone,`email`=:email WHERE `id` = '$orgID'";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':orgname', $orgname,  PDO::PARAM_STR);
+            $stmt->bindParam(':orgtype', $orgtype,  PDO::PARAM_STR);
+            $stmt->bindParam(':INN', $INN,  PDO::PARAM_STR);
+            $stmt->bindParam(':KPP', $KPP,  PDO::PARAM_STR);
+            $stmt->bindParam(':phone', $phone,  PDO::PARAM_STR);
+            $stmt->bindParam(':email', $email,  PDO::PARAM_STR);
+
+            $check_already = API::check_org($orgtype, $INN, $KPP);
+            if (!$check_already) {
+                $stmt->execute();
+                $result_json = array('success' => 'Информация об организации с названием ' . $orgname . ' успешно обновлена!');
                 return json_encode($result_json, JSON_UNESCAPED_UNICODE);
             } else {
                 $result_json = array('already' => 'Организация с таким ИНН или КПП уже зарегистрирована в базе');
