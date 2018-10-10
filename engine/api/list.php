b5bd957d73dfb3917c1dc733c880e6bae27537c2
Big Energy <andre90094@gmail.com>
2018-10-11 00:04:13 +0300

    Переход на ООП

--- engine/api/list.php
+++ engine/api/list.php
@@ -1,35 +1,8 @@
 <?php
-// ajax обработчик списка всех | by Big_Energy
 
-// показ ошибок
-//ini_set('display_errors',1);
-//error_reporting(E_ALL);
-// подключаем конфиг бд
-$configs = include('../config.php');
+include_once '../classes/API.class.php';
 
-$getid = $_GET['id'];
+$list_class = new API();
+$list = $list_class->list_org($_GET['id']);
 
-// подключаемся к базе
-
-$dsn = "mysql:host=$configs->host;dbname=$configs->database;charset=utf8";
-
-// Параметры задают что в качестве ответа получаем ассоциативный массив
-$opt = array(
-    PDO::ATTR_ERRMODE  => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-);
-
-// Проверка корректности подключения
-try { $pdo = new PDO($dsn, $configs->username, $configs->pass, $opt); }
-catch (PDOException $e) { die('Подключение не удалось: ' . $e->getMessage()); }
-
-
-if($getid != NULL) {
-    $array = $pdo->query("SELECT * FROM organizations WHERE id = '$getid'")->fetchAll(PDO::FETCH_ASSOC);
-} else {
-    $array = $pdo->query("SELECT * FROM organizations")->fetchAll(PDO::FETCH_ASSOC);
-}
-
-echo json_encode($array,JSON_UNESCAPED_UNICODE);
-
-?>
+echo $list;
